e423364d82cd148f49bdb12232eacbc8b2124c4f
Kalle <kalle@kalle-System-Product-Name.(none)>
2013-04-14 18:21:56 +0300

    tehtävät 1 ja 2 tehty

--- viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
+++ viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
@@ -1,4 +1,3 @@
-
 package ohtu;
 
 import org.openqa.selenium.By;
@@ -7,26 +6,104 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Tester {
+
     public static void main(String[] args) {
         WebDriver driver = new HtmlUnitDriver();
 
         driver.get("http://localhost:8080");
-        System.out.println( driver.getPageSource() );
-        WebElement element = driver.findElement(By.linkText("login"));       
-        element.click(); 
-        
+        System.out.println(driver.getPageSource());
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+
         System.out.println("==");
-        
-        System.out.println( driver.getPageSource() );
+        System.out.println(driver.getPageSource());
+
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
         element.submit();
-        
+
         System.out.println("==");
-        System.out.println( driver.getPageSource() );
-        
+        System.out.println(driver.getPageSource());
+
+        //Logout
+
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
+        //System.out.println("==");
+        //System.out.println(driver.getPageSource());
+
+        //LogIn wrong PW
+        element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akke");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+
+        System.out.println("==");
+        System.out.println(driver.getPageSource());
+
+        //LogIn nonexsisting user
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka2");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+
+        //========================
+
+        element = driver.findElement(By.linkText("back to home"));
+        element.click();
+
+        System.out.println("==");
+        System.out.println(driver.getPageSource());
+
+        //rekisteröidy
+
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("kalle");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("ellak123");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("ellak123");
+        element = driver.findElement(By.name("add"));
+        element.click();
+
+        System.out.println("== kun käyttäjä luotu:");
+        System.out.println(driver.getPageSource());
+
+
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
+        System.out.println("==");
+        System.out.println(driver.getPageSource());
+
+        element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("kalle");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("ellak123");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+
+        System.out.println("==");
+        System.out.println(driver.getPageSource());
+
     }
 }
